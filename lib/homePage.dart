1137bf5e90ce140a2a3e0268060b23628151f445
Murilo Silva <murilo.alex.silva@gmail.com>
2020-09-09 09:59:36 -0300

    .

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -9,9 +9,17 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.lime[100],
       appBar: AppBar(
-        backgroundColor: Colors.lime[200],
+        centerTitle: true,
+        backgroundColor: Colors.lime[500],
+        title: Text(
+          "I am rich",
+          style: TextStyle(
+              fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white70),
+        ),
       ),
+      body: Center(child: Image.network('https://picsum.photos/250?image=9')),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:i_am_rich/homePage.dart';
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+main(List<String> args) {
+  runApp(
+    MaterialApp(
       home: HomePage(),
-    );
-  }
+    ),
+  );
 }
